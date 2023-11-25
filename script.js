e88ae3016c7ca06a33e5c0f016c00fb5eb5c717a
Sagor Mohammed Yousuf <smyousuf630@gmail.com>
2023-11-25 11:11:26 +0600

    fem-advice-generator-app

---
+++ script.js
@@ -0,0 +1,15 @@
+const id = document.querySelector(".id-name");
+const text = document.querySelector(".card-text");
+const btn = document.querySelector(".btn");
+
+async function generate () {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+
+    id.textContent = data.slip.id;
+    text.textContent = `${data.slip.advice}`;
+}
+
+btn.onclick = () => {
+    generate()
+}
